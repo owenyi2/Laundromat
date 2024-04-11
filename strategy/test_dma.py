fixed incorrect order for filling the cache in ema, sma, sma_r. breaking dma into its own test, still need to fix some bits of it.

strategy/test_dma.py
@@ -76,7 +76,7 @@ def avg(values: list) -> int:
 
 def SMA_standard(prices: list, period: int) -> list:
     if not prices or period <= 0:
-        return 0
+        return -1
 
     avgs = []
     for i in range(len(prices)):
@@ -96,6 +96,69 @@ def EMA_standard(prices: list, period: int, smoothing: int=2) -> list:
         avgs.append((prices[i] * alpha) + (prices[i-1] * (1 - alpha)))
 
     return avgs
+
+# DMA Implementation
+# thanku dickson...
+
+def _DMA_wma(prices: list, period: int) -> list:
+    if not prices or period <= 0 or period > len(prices):
+        return -1
+
+    avgs = []
+    weights = [i + 1 for i in range(period)][::-1]
+    for i in range(len(prices)):
+        if i < window:
+            wma_values.append(sum(prices[:i+1]) / (i+1))
+            continue
+        weighted_sum = sum([prices[i - j] * weights[j] for j in range(period)])
+        avgs.append(weighted_sum / sum(weights))
+
+    return avgs
+
+def _DMA_hma(prices: list, period: int) -> list:
+    if not prices or period <= 0 or period > len(prices):
+        return -1
+    return _DMA_wma(2 * avg(_DMA_wma(prices, int(period / 2))) - avg(_DMA_wma(prices, period)), int(math.sqrt(period)))
+
+def _DMA_ehma(prices: list, period: int) -> list:
+    if not prices or period <= 0 or period > len(prices):
+        return -1
+    return EMA_standard(2 * avg(EMA_standard(prices, int(period / 2))) - avg(EMA_standard(prices, period)), int(math.sqrt(period)))
+
+# Translated from: https://www.tradingview.com/script/8MEEEGWl-Dickinson-Moving-Average-DMA/
+def DMA_v3(prices: list, wma_mode=True) -> list:
+    # inputs
+    hulllength = 7
+    emalength = 20
+    emagainlimit = 50
+    leasterror = 1000000.0
+
+    src = prices[-1]
+
+    #dma
+    alpha = 2 / (emalength + 1)
+    e0 = 0.0
+    e0 = alpha * src + (1 - alpha) * e0[1] # CHECK
+
+    gain = 0.0
+    bestgain = 0.0
+    error = 0.0
+    ec = 0.0
+
+    avgs = []
+    for i in range(emagainlimit):
+        gain = i / 10
+        ec = alpha * (e0 + gain * (src - ec)) + (1 - alpha) * ec # CHECK
+        error = abs(src - ec) # CHECK
+        if error < leasterror:
+            leasterror = error
+            bestgain = gain
+
+    ec = alpha * (e0 + bestgain * (src - ec)) + (1 - alpha) * ec # CHECK
+
+    if wma_mode:
+        return (ec + avg(_DMA_hma(prices, hulllength))) / 2
+    return (ec + avg(_DMA_ehma(prices, hulllength))) / 2
 
 
 class Logger:
@@ -278,11 +341,8 @@ class Trader:
         else:
             midprice_measurement = np.clip(midprice, previous_midprice - 2, previous_midprice + 2) # clip outliers
         #fair_value = self.compute_starfruit_fair_value(self.traderData["STARFRUIT"]["KF_state"], midprice_measurement)
-        ma_period = 5
-        if len(self.sf_ma_cache) == ma_period:
-            self.sf_ma_cache.pop(0)
         self.sf_ma_cache.append(midprice)
-        fair_value = avg(EMA_standard(self.sf_ma_cache, ma_period))
+        fair_value = DMA_v3(self.sf_ma_cache)
 
         print(f"fair,{fair_value}")
         print(f"midprice,{midprice}")
@@ -343,6 +403,6 @@ class Trader:
         orders["STARFRUIT"] = self.compute_starfruit_order(state)
 
         traderData = json.dumps(self.traderData)
-        #logger.flush(state, orders, conversions, traderData)
+        logger.flush(state, orders, conversions, traderData)
         return orders, conversions, traderData
 

strategy/test_sma.py
@@ -266,9 +266,9 @@ class Trader:
             midprice_measurement = np.clip(midprice, previous_midprice - 2, previous_midprice + 2) # clip outliers
         #fair_value = self.compute_starfruit_fair_value(self.traderData["STARFRUIT"]["KF_state"], midprice_measurement)
         ma_period = 5
-        self.sf_ma_cache.append(midprice)
         if len(self.sf_ma_cache) == ma_period:
             self.sf_ma_cache.pop(0)
+        self.sf_ma_cache.append(midprice)
         fair_value = avg(SMA_standard(self.sf_ma_cache, ma_period))
 
         print(f"fair,{fair_value}")